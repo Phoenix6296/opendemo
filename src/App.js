3aed9815f52f1dc308328942b60f376bca23b597
Krishna Biswakarma <phoenix@Krishnas-MacBook-Air.local>
2023-03-03 23:56:22 +0530

    Removing unecessary js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { useEffect } from 'react';
 import styles from './App.module.css';
 import Navbar from './Components/Navbar/Navbar';
-import FirstPage from './Components/Pages/First';
-import SecondPage from './Components/Pages/Second';
-import ThirdPage from './Components/Pages/Third';
-import FourthPage from './Components/Pages/Fourth';
-import FifthPage from './Components/Pages/Fifth';
-import SixthPage from './Components/Pages/Sixth';
+import Page from './Components/Pages/Page';
+import data from './Data/data.json'
 
-
+// const data = [
+//   {
+//     title1: 'Welcome To',
+//     title2: 'OpeninApp',
+//     videoURL1: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/Welcome%20to%20OpeninApp.webm',
+//   },
+//   {
+//     title1: 'A Super Tool',
+//     title2: 'For Creators',
+//     videoURL2: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/A%20Growth%20Tool%20for%20Creators.webm',
+//   },
+//   {
+//     title1: 'That helps you',
+//     title2: 'Grow on',
+//     videoURL1: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/That%20help%20grow%20on.webm',
+//   },
+//   {
+//     title1: 'Get Better',
+//     title2: 'Brand Deals',
+//     videoURL2: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/And%20get%20better%20brand%20deals.webm',
+//   },
+//   {
+//     title1: 'Loved by',
+//     title2: ['290+', <br />, 'Creator'],
+//     videoURL1: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/Already%20Loved%20by.webm'
+//   },
+//   {
+//     title1: 'Ready To Get',
+//     title2: 'Started?',
+//     videoURL2: 'https://storage.googleapis.com/openinapp-prod-assets/BG%20videos%20Webm/Ready%20to%20get%20onboard.webm',
+//     isButton: true
+//   },
+// ]
 const App = () => {
   useEffect(() => {
     const stickySections = [...document.querySelectorAll(`.${styles.sticky}`)];
@@ -32,12 +60,11 @@ const App = () => {
     <div className={styles.app}>
       <Navbar />
       <div className={styles.page_container}>
-        <FirstPage />
-        <SecondPage />
-        <ThirdPage />
-        <FourthPage />
-        <FifthPage />
-        <SixthPage />
+        {
+          data.map((item) => {
+            return <Page key={Math.random()} title1={item.title1} title2={item.title2} videoURL1={item.videoURL1} videoURL2={item.videoURL2} isButton={item.isButton} />
+          })
+        }
       </div>
     </div>
   );
